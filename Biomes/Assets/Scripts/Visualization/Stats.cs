7975006de545cfe3e045986a2d6663cdbc8ac15c
JasperDre <jasperdre@gmail.com>
2019-10-19 11:29:36 +0200

    Add stats UI and camera controller

---
+++ Biomes/Assets/Scripts/Visualization/Stats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour
+{
+    [SerializeField] private Slider myHungerSlider;
+    [SerializeField] private Slider myThirstSlider;
+
+    private Animal myAnimal;
+
+    private void Awake()
+    {
+        myAnimal = GetComponent<Animal>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myHungerSlider.value = myAnimal.hunger;
+        myThirstSlider.value = myAnimal.thirst;
+    }
+}
